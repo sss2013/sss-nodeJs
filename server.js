f2db4f81341ce502205f5ba91604dbd54cccd06e
sss2013 <kshi9478@gmail.com>
2025-09-12 23:04:37 +0900

    ✨add update&delete& popular list function

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let db
 require('dotenv').config()
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 
 const url = `mongodb+srv://${dbUser}:${dbPass}@cluster0.sx8zk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 new MongoClient(url).connect().then((client) => {
@@ -22,6 +22,8 @@ app.use(express.static(__dirname + '/public'))
 app.listen(8080, () => {
     console.log('Server is running on port 8080')
 })
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home.ejs');
@@ -47,8 +49,113 @@ app.get('/add', (req, res) => {
 
 app.post('/add', async (req, res) => {
     const { artist, name } = req.body;
-    await db.collection('songs').insertOne({ artist: artist, name: name });
-    let result = await db.collection('songs').find().toArray()
-    res.render('list.ejs', { songs: result })
+    if (artist === "" || name === "") {
+        return res.status(400).send('Artist and name are required');
+    }
+
+    try {
+        await db.collection('songs').insertOne({ artist: artist, name: name });
+        let result = await db.collection('songs').find().toArray()
+        res.redirect('/list');
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Error inserting song');
+    }
+
 })
 
+app.get('/detail/:id', async (req, res) => {
+    try {
+        let result = await db.collection('songs').findOne({
+            _id: new ObjectId(req.params.id)
+        })
+        if (result == null) {
+            return res.status(404).send('Song not found')
+        }
+        res.render('detail.ejs', { song: result })
+    } catch (e) {
+        console.log(e)
+        return res.status(400).send('Invalid ID format')
+    }
+})
+
+app.get('/update/:id', async (req, res) => {
+    try {
+        console.log(req.params.id)
+        let item = await db.collection('songs').findOne({
+            _id: new ObjectId(req.params.id)
+        })
+        if (item == null) {
+            return res.status(404).send('Song not found')
+        }
+        res.render('updateSong.ejs', { song: item })
+    } catch (e) {
+        console.log(e)
+        return res.status(400).send('Invalid ID format')
+    }
+})
+
+app.put('/update', async (req, res) => {
+
+    const { id, artist, name } = req.body;
+    if (id === "" || artist === "" || name === "") {
+        return res.status(400).send('Artist and name are required');
+    }
+    try {
+        await db.collection('songs').updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { artist: artist, name: name } }
+        );
+        res.redirect(`/detail/${id}`);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Error updating song');
+    }
+})
+
+app.put('/like', async (req, res) => {
+    const { id } = req.body;
+    if (id === "") {
+        return res.status(400).send('ID is required');
+    }
+    try {
+        await db.collection('songs').updateOne(
+            { _id: new ObjectId(id) },
+            { $inc: { like: 1 } }
+        );
+        res.redirect(`/detail/${id}`);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Error liking song');
+    }
+})
+
+app.get('/popular', async (req, res) => {
+    try {
+        const result = await db.collection('songs').find(
+            { like: { $gte: 2 } }
+        ).sort({ like: -1 }).toArray()
+        res.render('popular.ejs', { songs: result })
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+app.delete('/delete', async (req, res) => {
+    try {
+        const { id } = req.body;
+        if (id === "") {
+            return res.status(400).send('ID is required');
+        }
+        await db.collection('songs').deleteOne(
+            { _id: new ObjectId(id) }
+        );
+        const result = await db.collection('songs').find().toArray()
+
+        return res.status(200).json({ songs: result });
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send('Error deleting song');
+    }
+})
